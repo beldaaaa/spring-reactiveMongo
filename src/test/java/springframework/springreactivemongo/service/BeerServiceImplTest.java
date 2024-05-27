a7acfca3538a5cf822b96f2a191c3cbe0b9dc407
beldaaaa <oldbanana@email.cz>
2024-05-27 09:45:24 +0200

    CRUD Beer and Customer tests implemented

--- src/test/java/springframework/springreactivemongo/service/BeerServiceImplTest.java
+++ src/test/java/springframework/springreactivemongo/service/BeerServiceImplTest.java
@@ -5,13 +5,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 import springframework.springreactivemongo.domain.Beer;
 import springframework.springreactivemongo.mapper.BeerMapper;
+import springframework.springreactivemongo.mapper.BeerMapperImpl;
 import springframework.springreactivemongo.model.BeerDTO;
 
 import java.math.BigDecimal;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
@@ -31,6 +35,7 @@ class BeerServiceImplTest {
 
     public static Beer helperBeer() {
         return Beer.builder()
+                .id("12345678987654321")
                 .beerName("RandomName")
                 .beerStyle("RandomStyle")
                 .upc("661")
@@ -39,15 +44,96 @@ class BeerServiceImplTest {
                 .build();
     }
 
+    public static BeerDTO helperBeerDTO() {
+        return new BeerMapperImpl().beerToBeerDTO(helperBeer());
+    }
+
     @Test
     void saveBeer() {
         AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        AtomicReference<BeerDTO> atomicDTO = new AtomicReference<>();
         Mono<BeerDTO> savedMono = beerService.saveBeer(Mono.just(beerDTO));
-        savedMono.subscribe(savedDto -> {
-            System.out.println(savedDto.getBeerName());
+
+        savedMono.subscribe(savedDTO -> {
             atomicBoolean.set(true);
+            atomicDTO.set(savedDTO);
         });
         await().untilTrue(atomicBoolean);
+
+        BeerDTO persistedDTO = atomicDTO.get();
+        assertThat(persistedDTO).isNotNull();
+        assertThat(persistedDTO.getId()).isEqualTo(beerDTO.getId());
+    }
+
+    @Test
+    void updateBeer() {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        AtomicReference<BeerDTO> atomicDTO = new AtomicReference<>();
+        Mono<BeerDTO> savedMono = beerService.saveBeer(Mono.just(beerDTO));
+        final String updatedBeerName = "JustAnotherName";
+
+        savedMono.subscribe(savedDTO -> {
+            atomicBoolean.set(true);
+            atomicDTO.set(savedDTO);
+        });
+        await().untilTrue(atomicBoolean);
+
+        BeerDTO persistedDTO = atomicDTO.get();
+        assertThat(persistedDTO).isNotNull();
+        assertThat(persistedDTO.getId()).isEqualTo(beerDTO.getId());
+
+        persistedDTO.setBeerName("JustAnotherName");
+        AtomicReference<BeerDTO> updatedAtomicDTO = new AtomicReference<>();
+        Mono<BeerDTO> updatedMono = beerService.updateBeer(persistedDTO.getId(), persistedDTO);
+
+        updatedMono.subscribe(updatedAtomicDTO::set);
+        await().until(() -> updatedAtomicDTO.get() != null);
+
+        BeerDTO updatedDTO = updatedAtomicDTO.get();
+        assertThat(updatedDTO).isNotNull();
+        assertThat(updatedDTO.getBeerName()).isEqualTo(updatedBeerName);
+    }
+
+    @Test
+    void patchBeer() {
+        final String patchedBeerName = "JustAnotherRandomNameAfterPatch";
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        AtomicReference<BeerDTO> atomicDTO = new AtomicReference<>();
+        Mono<BeerDTO> savedMono = beerService.saveBeer(Mono.just(beerDTO));
+
+        savedMono.subscribe(savedDTO -> {
+            atomicBoolean.set(true);
+            atomicDTO.set(savedDTO);
+        });
+        await().untilTrue(atomicBoolean);
+
+        BeerDTO persistedDTO = atomicDTO.get();
+        assertThat(persistedDTO).isNotNull();
+        assertThat(persistedDTO.getId()).isEqualTo(beerDTO.getId());
+
+        persistedDTO.setBeerName("JustAnotherRandomNameAfterPatch");
+        AtomicReference<BeerDTO> patchedAtomicDTO = new AtomicReference<>();
+        Mono<BeerDTO> patchedMono = beerService.patchBeer(persistedDTO.getId(), persistedDTO);
+
+        patchedMono.subscribe(patchedAtomicDTO::set);
+        await().until(() -> patchedAtomicDTO.get() != null);
+
+        BeerDTO patchedDTO = patchedAtomicDTO.get();
+        assertThat(patchedDTO).isNotNull();
+        assertThat(patchedDTO.getBeerName()).isEqualTo(patchedBeerName);
+    }
+
+    @Test
+    void deleteBeer() {
+        BeerDTO beerToDelete = helperBeerDTO();
+        beerService.deleteBeer(beerToDelete.getId()).subscribe();
+
+        Mono<BeerDTO> expectedEmptyBeerMono = beerService.findBeerById(beerToDelete.getId());
+
+        StepVerifier.create(expectedEmptyBeerMono)
+                .expectNextCount(0)
+                .verifyComplete();
     }
 
 }
+
--- src/test/java/springframework/springreactivemongo/service/CustomerServiceImplTest.java
+++ src/test/java/springframework/springreactivemongo/service/CustomerServiceImplTest.java
@@ -5,12 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 import springframework.springreactivemongo.domain.Customer;
 import springframework.springreactivemongo.mapper.CustomerMapper;
+import springframework.springreactivemongo.mapper.CustomerMapperImpl;
 import springframework.springreactivemongo.model.CustomerDTO;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
@@ -29,19 +33,100 @@ class CustomerServiceImplTest {
 
     public static Customer helperCustomer() {
         return Customer.builder()
+                .id("99999999")
                 .customerName("Jolanda")
                 .build();
     }
 
+    public static CustomerDTO CustomerDTO() {
+        return new CustomerMapperImpl().customerToCustomerDto(helperCustomer());
+    }
+
     @Test
-    void saveCustomer() {
+    void saveBeer() {
         AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        AtomicReference<CustomerDTO> atomicDTO = new AtomicReference<>();
         Mono<CustomerDTO> savedMono = customerService.saveCustomer(Mono.just(customerDTO));
-        savedMono.subscribe(savedDto -> {
-            System.out.println(savedDto.getCustomerName());
+
+        savedMono.subscribe(savedDTO -> {
             atomicBoolean.set(true);
+            atomicDTO.set(savedDTO);
         });
         await().untilTrue(atomicBoolean);
+
+        CustomerDTO persistedDTO = atomicDTO.get();
+        assertThat(persistedDTO).isNotNull();
+        assertThat(persistedDTO.getId()).isEqualTo(customerDTO.getId());
+    }
+
+    @Test
+    void updateBeer() {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        AtomicReference<CustomerDTO> atomicDTO = new AtomicReference<>();
+        Mono<CustomerDTO> savedMono = customerService.saveCustomer(Mono.just(customerDTO));
+        final String updatedBeerName = "JustAnotherName";
+
+        savedMono.subscribe(savedDTO -> {
+            atomicBoolean.set(true);
+            atomicDTO.set(savedDTO);
+        });
+        await().untilTrue(atomicBoolean);
+
+        CustomerDTO persistedDTO = atomicDTO.get();
+        assertThat(persistedDTO).isNotNull();
+        assertThat(persistedDTO.getId()).isEqualTo(customerDTO.getId());
+
+        persistedDTO.setCustomerName("JustAnotherName");
+        AtomicReference<CustomerDTO> updatedAtomicDTO = new AtomicReference<>();
+        Mono<CustomerDTO> updatedMono = customerService.updateCustomer(persistedDTO.getId(), persistedDTO);
+
+        updatedMono.subscribe(updatedAtomicDTO::set);
+        await().until(() -> updatedAtomicDTO.get() != null);
+
+        CustomerDTO updatedDTO = updatedAtomicDTO.get();
+        assertThat(updatedDTO).isNotNull();
+        assertThat(updatedDTO.getCustomerName()).isEqualTo(updatedBeerName);
+    }
+
+    @Test
+    void patchBeer() {
+        final String patchedBeerName = "JustAnotherRandomNameAfterPatch";
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        AtomicReference<CustomerDTO> atomicDTO = new AtomicReference<>();
+        Mono<CustomerDTO> savedMono = customerService.saveCustomer(Mono.just(customerDTO));
+
+        savedMono.subscribe(savedDTO -> {
+            atomicBoolean.set(true);
+            atomicDTO.set(savedDTO);
+        });
+        await().untilTrue(atomicBoolean);
+
+        CustomerDTO persistedDTO = atomicDTO.get();
+        assertThat(persistedDTO).isNotNull();
+        assertThat(persistedDTO.getId()).isEqualTo(customerDTO.getId());
+
+        persistedDTO.setCustomerName("JustAnotherRandomNameAfterPatch");
+        AtomicReference<CustomerDTO> patchedAtomicDTO = new AtomicReference<>();
+        Mono<CustomerDTO> patchedMono = customerService.patchCustomer(persistedDTO.getId(), persistedDTO);
+
+        patchedMono.subscribe(patchedAtomicDTO::set);
+        await().until(() -> patchedAtomicDTO.get() != null);
+
+        CustomerDTO patchedDTO = patchedAtomicDTO.get();
+        assertThat(patchedDTO).isNotNull();
+        assertThat(patchedDTO.getCustomerName()).isEqualTo(patchedBeerName);
+    }
+
+    @Test
+    void deleteBeer() {
+        CustomerDTO beerToDelete = CustomerDTO();
+        customerService.deleteCustomer(beerToDelete.getId()).subscribe();
+
+        Mono<CustomerDTO> expectedEmptyBeerMono = customerService.findCustomerById(beerToDelete.getId());
+
+        StepVerifier.create(expectedEmptyBeerMono)
+                .expectNextCount(0)
+                .verifyComplete();
     }
 
 
