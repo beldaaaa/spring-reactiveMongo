3976256544307c264a302e6dd7d4ada38ef6a152
beldaaaa <oldbanana@email.cz>
2024-05-29 15:03:28 +0200

    Added Beer and Customer endpoint tests (missing impl => not green yet)

---
+++ src/test/java/springframework/springreactivemongo/web/fn/BeerEndpointTest.java
@@ -0,0 +1,168 @@
+package springframework.springreactivemongo.web.fn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+import springframework.springreactivemongo.domain.Beer;
+import springframework.springreactivemongo.model.BeerDTO;
+import springframework.springreactivemongo.service.BeerServiceImplTest;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class BeerEndpointTest {
+    private static final int TEST_ID = 132657456;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    public BeerDTO helperBeer() {
+        FluxExchangeResult<BeerDTO> beerDTOFluxExchangeResult = webTestClient.post().uri(BeerRouterConfig.BEER_PATH)
+                .body(Mono.just(BeerServiceImplTest.helperBeer()), BeerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .returnResult(BeerDTO.class);
+
+        List<String> location = beerDTOFluxExchangeResult.getResponseHeaders().get("Location");
+
+        return webTestClient.get().uri(BeerRouterConfig.BEER_PATH)
+                .exchange().returnResult(BeerDTO.class).getResponseBody().blockFirst();
+    }
+
+    @Test
+    void createBeer() {
+        webTestClient.post().uri(BeerRouterConfig.BEER_PATH)
+                .body(Mono.just(BeerServiceImplTest.helperBeer()), BeerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().location("http://localhost:8080/api/v2/beer/4");
+    }
+
+    @Test
+    void createBeerWithBadData() {
+        Beer testBeer = BeerServiceImplTest.helperBeer();
+        testBeer.setBeerName("");
+
+        webTestClient.post().uri(BeerRouterConfig.BEER_PATH)
+                .body(Mono.just(testBeer), BeerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    @Order(4)
+    void updateBeerWithBadRequest() {
+        BeerDTO testBeer = helperBeer();
+        testBeer.setBeerStyle("");
+
+        webTestClient.put()
+                .uri(BeerRouterConfig.BEER_PATH_ID, testBeer)
+                .body(Mono.just(testBeer), BeerDTO.class)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    void updateBeerNotFound() {
+        webTestClient.put()
+                .uri(BeerRouterConfig.BEER_PATH_ID, TEST_ID)
+                .body(Mono.just(BeerServiceImplTest.helperBeer()), BeerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @Order(3)
+    void updateBeerFound() {
+
+        BeerDTO beerDTO = helperBeer();
+
+        webTestClient.put()
+                .uri(BeerRouterConfig.BEER_PATH_ID, beerDTO.getId())
+                .body(Mono.just(beerDTO), BeerDTO.class)
+                .exchange()
+                .expectStatus().isNoContent();
+    }
+
+    @Test
+    void patchBeerNotFound() {
+        webTestClient.patch()
+                .uri(BeerRouterConfig.BEER_PATH_ID, TEST_ID)
+                .body(Mono.just(BeerServiceImplTest.helperBeer()), BeerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void patchBeerFound() {
+        BeerDTO beerDTO = helperBeer();
+
+        webTestClient.patch()
+                .uri(BeerRouterConfig.BEER_PATH_ID, beerDTO.getId())
+                .body(Mono.just(beerDTO), BeerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void deleteBeerNotFound() {
+        webTestClient.delete()
+                .uri(BeerRouterConfig.BEER_PATH_ID, TEST_ID)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @Order(100)
+    void deleteBeerFound() {
+        BeerDTO beerDTO = helperBeer();
+
+        webTestClient.delete()
+                .uri(BeerRouterConfig.BEER_PATH_ID, beerDTO.getId())
+                .exchange()
+                .expectStatus()
+                .isNoContent();
+    }
+
+
+    @Test
+    void findByIdNotFound() {
+        webTestClient.get().uri(BeerRouterConfig.BEER_PATH_ID, TEST_ID)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @Order(1)
+    void findByIdFound() {
+        BeerDTO beerDTO = helperBeer();
+
+        webTestClient.get().uri(BeerRouterConfig.BEER_PATH_ID, beerDTO.getId())
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody(BeerDTO.class);
+    }
+
+    @Test
+    @Order(2)
+    void beerList() {
+        webTestClient.get().uri(BeerRouterConfig.BEER_PATH)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody().jsonPath("$.size()", hasSize(greaterThan(1)));
+    }
+
+}
---
+++ src/test/java/springframework/springreactivemongo/web/fn/CustomerEndpointTest.java
@@ -0,0 +1,168 @@
+package springframework.springreactivemongo.web.fn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+import springframework.springreactivemongo.domain.Customer;
+import springframework.springreactivemongo.model.CustomerDTO;
+import springframework.springreactivemongo.service.CustomerServiceImplTest;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class CustomerEndpointTest {
+    private static final int TEST_ID = 132657456;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    public CustomerDTO helperCustomer() {
+        FluxExchangeResult<CustomerDTO> customerDTOFluxExchangeResult = webTestClient.post().uri(CustomerRouterConfig.CUSTOMER_PATH)
+                .body(Mono.just(CustomerServiceImplTest.helperCustomer()), CustomerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .returnResult(CustomerDTO.class);
+
+        List<String> location = customerDTOFluxExchangeResult.getResponseHeaders().get("Location");
+
+        return webTestClient.get().uri(CustomerRouterConfig.CUSTOMER_PATH)
+                .exchange().returnResult(CustomerDTO.class).getResponseBody().blockFirst();
+    }
+
+    @Test
+    void createCustomer() {
+        webTestClient.post().uri(CustomerRouterConfig.CUSTOMER_PATH)
+                .body(Mono.just(CustomerServiceImplTest.helperCustomer()), CustomerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().location("http://localhost:8080/api/v3/customer/4");
+    }
+
+    @Test
+    void createCustomerWithBadData() {
+        Customer testCustomer = CustomerServiceImplTest.helperCustomer();
+        testCustomer.setCustomerName("");
+
+        webTestClient.post().uri(CustomerRouterConfig.CUSTOMER_PATH)
+                .body(Mono.just(testCustomer), CustomerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    @Order(4)
+    void updateCustomerWithBadRequest() {
+        CustomerDTO testCustomer = helperCustomer();
+        testCustomer.setCustomerName("");
+
+        webTestClient.put()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH_ID, testCustomer)
+                .body(Mono.just(testCustomer), CustomerDTO.class)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    void updateCustomerNotFound() {
+        webTestClient.put()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH_ID, TEST_ID)
+                .body(Mono.just(CustomerServiceImplTest.helperCustomer()), CustomerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @Order(3)
+    void updateCustomerFound() {
+
+        CustomerDTO customerDTO = helperCustomer();
+
+        webTestClient.put()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH_ID, customerDTO.getId())
+                .body(Mono.just(customerDTO), CustomerDTO.class)
+                .exchange()
+                .expectStatus().isNoContent();
+    }
+
+    @Test
+    void patchCustomerNotFound() {
+        webTestClient.patch()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH_ID, TEST_ID)
+                .body(Mono.just(CustomerServiceImplTest.helperCustomer()), CustomerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void patchCustomerFound() {
+        CustomerDTO customerDTO = helperCustomer();
+
+        webTestClient.patch()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH_ID, customerDTO.getId())
+                .body(Mono.just(customerDTO), CustomerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void deleteCustomerNotFound() {
+        webTestClient.delete()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH_ID, TEST_ID)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @Order(100)
+    void deleteCustomerFound() {
+        CustomerDTO customerDTO = helperCustomer();
+
+        webTestClient.delete()
+                .uri(CustomerRouterConfig.CUSTOMER_PATH_ID, customerDTO.getId())
+                .exchange()
+                .expectStatus()
+                .isNoContent();
+    }
+
+    @Test
+    void findByIdNotFound() {
+        webTestClient.get().uri(CustomerRouterConfig.CUSTOMER_PATH_ID, TEST_ID)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @Order(1)
+    void findByIdFound() {
+        CustomerDTO customerDTO = helperCustomer();
+
+        webTestClient.get().uri(CustomerRouterConfig.CUSTOMER_PATH_ID, customerDTO.getId())
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody(CustomerDTO.class);
+    }
+
+    @Test
+    @Order(2)
+    void customerList() {
+        webTestClient.get().uri(CustomerRouterConfig.CUSTOMER_PATH)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody().jsonPath("$.size()", hasSize(greaterThan(1)));
+    }
+
+}
+
