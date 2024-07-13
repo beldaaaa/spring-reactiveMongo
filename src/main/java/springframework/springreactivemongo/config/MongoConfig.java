aa8725e7499f5dc4907ac9edcbd6b1bb7ad9e15c
beldaaaa <oldbanana@email.cz>
2024-07-13 11:59:50 +0200

    Docker image preparation

--- src/main/java/springframework/springreactivemongo/config/MongoConfig.java
+++ src/main/java/springframework/springreactivemongo/config/MongoConfig.java
@@ -1,14 +1,23 @@
 package springframework.springreactivemongo.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 
+import static java.util.Collections.singletonList;
+
 
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
+
+    @Value("${mongoooo.host}")
+    private String mongoHost;
 
     @Bean
     MongoClient mongoClient() {
@@ -20,11 +29,11 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
         return "Mongoooo";
     }
 //settings for Docker
-//    @Override
-//    protected void configureClientSettings(MongoClientSettings.Builder builder) {
-//        builder.credential(MongoCredential.
-//                        createCredential("root", "admin", "example".toCharArray()))
-//                .applyToClusterSettings(settings ->
-//                        settings.hosts(Collections.singletonList(new ServerAddress("localhost", 27017))));
-//    }
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder.credential(MongoCredential.
+                        createCredential("root", "admin", "example".toCharArray()))
+                .applyToClusterSettings(settings ->
+                        settings.hosts(singletonList(new ServerAddress(mongoHost))));
+    }
 }
